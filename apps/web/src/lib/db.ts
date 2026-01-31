699f22f6de77b3f349beef4d02034b629c399c58
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-31 14:43:58 +0100

    Revert "update"
    This reverts commit 6c203445d9a51de46f1826c10e84a7e4dd5fa51b.

--- apps/web/src/lib/db.ts
+++
@@ -1,4 +0,0 @@
-// Re-export database from @portfolio/db package
-export { db } from "@portfolio/db";
-export * from "@portfolio/db";
-
--- packages/crypto/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { generateSalt, deriveKey, deriveKeyHex } from "./kdf";
-export { encrypt, encryptObject, type EncryptedData } from "./encrypt";
-export { decrypt, decryptObject, type DecryptInput } from "./decrypt";
-
